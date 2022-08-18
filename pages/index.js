ca0d036755742be62d9d78253c8d1db5be946d84
Alexey Lukin <alexey.lukin@da-14.com>
2022-08-19 00:43:27 +0300

    part 2

--- pages/index.js
+++ pages/index.js
@@ -26,6 +26,22 @@ const holesCount = 4;
 if (sizeX * sizeY < holesCount) throw(new Error("wrong config"));
 if (holesCount === 0) throw(new Error("wrong config"));
 
+// uniform distribution = not exactly random?
+// looking at example game it seems to be random
+// so it will be random for v1
+// probably later some conditions may be added to prevent spawning all holes at the same area close to each other?
+// or they may be dynamically spawned to prevent lose on first click on different difficulties?
+// or condition to reveal large area on first click on easy can be added?
+// etc...
+let generatedCount = 0;
+while (generatedCount < holesCount) {
+  let x = Math.floor(Math.random() * sizeX)
+  let y = Math.floor(Math.random() * sizeY)
+  if (!board[x][y].isHole) {
+    board[x][y].isHole = true;
+    generatedCount++;
+  }
+}
 
 export default function Home() {
   return (
